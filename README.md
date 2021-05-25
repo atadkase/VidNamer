450c99b92c9c3054bcbbb8f4cd00deedb4f09982
atadkase <ashutosh.tadkase@gmail.com>
2021-05-24 18:49:48 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,69 @@
+# VidNamer
+## A tool for renaming GoPro® video files
+The [naming schema used by GoPro® cameras](https://community.gopro.com/t5/en/GoPro-Camera-File-Naming-Convention/ta-p/390220) is unintuitive for video editing. After agonizing over the 
+filenames for a long time when working with a larger project, I decided to write a script to make the 
+names more useable. This tool is an extension of that script. 
+Cameras older than HERO8 Black are not supported. Please file an issue if you need it.
+
+## Installation
+I recommend using the script directly with Python with the following command:
+```shell
+py vidnamer.py
+```
+If you do not have python installed, please follow instructions here: [Download Python](https://www.python.org/downloads/)
+
+It will launch an interactive GUI which can be used to automatically rename your video files.
+I've written this to be OS independent and I've tested it manually on Windows 10. YMMV for other platforms.
+
+If you do not want to install python, you can grab a compiled executable zip here: [Download VidNamer for Windows](https://github.com/atadkase/VidNamer/suites/2806735111/artifacts/62429273)
+
+Github does not provide a permanent URL for the latest artifact, so I have to update this link manually. 
+
+## Using the tool
+### Backup all files before using this tool!
+I cannot stress the importance of backing up your files more. This tool is not written to production standards and may have bugs. Use it at your own risk.
+
+Now that we've gotten the disclaimer out of the way, we can talk about the operation of the tool.
+The tool has a simple UI:
+![VidNamer_ui](/screenshots/ui_pic.JPG "VidNamer UI")
+
+The `Browse Directory` button allows you to select the directory containing the videos. The tool will not process directories inside that directory. Please file an issue if you need recursion.
+
+You can then set each of the following options:
+
+- `Ask for custom prefix per video group`: For each new video group the tool detects, it will prompt for a custom prefix to add to the video name. The prefix will be suffixed by the chapter numbers from the source video files for getting the new names.
+- `Create a new directory per video group`: This will create a new directory for each new group and move the renamed files to that directory.
+- `Copy videos instead of moving`: I highly recommend keeping this box checked unless you have your files backed up somewhere. If you uncheck this, the tool will just rename the files, but if the filenames clash, it may overwrite existing files based on your selection. If you do not have any clashing names, it is much faster to disable this option.
+- `Use auto numbering scheme for filename conflicts`: Only disable this if you know that you want to overwrite existing files. If this option is enabled the tool will read the latest files in the target directory and autoincrement the chapter index to avoid filename conflicts.
+- `Ask before writing to an existing directory`: Will prompt the user when the target directory already exists.
+
+Once you've selected your target options, you can hit `Start Renaming!` and wait for the completion dialog box.
+
+## Contributing
+I am accepting contributions to this project. If you want to add a feature or fix a bug, please create a GitHub issue and a corresponding Pull Request from your fork. 
+
+## Donations
+If this tool helped you save time, please consider donating to any of the COVID-19 relief efforts in India. Every bit helps: [Donation Link](https://www.ketto.org/fundraiser/mission-oxygen-helping-hospitals-to-save-lives?payment=form)
+
+## Attributions
+A big thank you to [JackMcKew](https://github.com/JackMcKew) for the Pyinstaller-Windows Action which is being used to build the project's Windows executables.
+
+This product and/or service is not affiliated with, endorsed by or in any 
+way associated with GoPro Inc. or its products and services. GoPro, HERO 
+and their respective logos are trademarks or registered trademarks of GoPro, Inc.
+
+## License
+Copyright 2021 Ashutosh Tadkase
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
---
+++ src/vidnamer.py
@@ -0,0 +1,289 @@
+#!/usr/bin/env python
+"""
+VidNamer: Video renamer for GoPro® video files
+Copyright 2021 Ashutosh Tadkase
+
+License:
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+This product and/or service is not affiliated with, endorsed by or in any 
+way associated with GoPro Inc. or its products and services. GoPro, HERO 
+and their respective logos are trademarks or registered trademarks of GoPro, Inc.
+"""
+
+import os
+import tkinter as tk
+from tkinter import (
+    BooleanVar,
+    simpledialog,
+    messagebox,
+    filedialog,
+    ttk,
+    StringVar,
+    Label,
+    Entry,
+)
+import shutil
+
+
+class VidNamer:
+    def __init__(
+        self,
+        get_user_input_per_group=True,
+        copy_instead_of_move=False,
+        create_new_dir_per_group=True,
+        check_dir_merge=False,
+        check_automated_numbering=False,
+    ):
+        self.get_user_input_per_group = get_user_input_per_group
+        self.copy_instead_of_move = copy_instead_of_move
+        self.create_new_dir_per_group = create_new_dir_per_group
+        self.check_dir_merge = check_dir_merge
+        self.check_automated_numbering = check_automated_numbering
+
+    @staticmethod
+    def get_group_dict(
+        dirpath, group_regex=r"G[A-Z]([0-9][0-9])([0-9]+)", reverse_group=False
+    ):
+        """Get group dict inside the path"""
+        import re
+
+        f = [
+            filename
+            for filename in os.listdir(dirpath)
+            if os.path.isfile(os.path.join(dirpath, filename))
+            and filename.endswith(("mp4", "MP4"))
+        ]
+        group_dict = (
+            {}
+        )  # Dictionary containing group to chapter and file mapping. Useful for splitting groups and renaming.
+        for filename in f:
+            match = re.match(group_regex, filename)
+            if match:
+                chapter, group = match.groups()
+                if reverse_group:
+                    group, chapter = match.groups()
+                # TODO - Pitfall- should add dirpath too to the filename
+                if group not in group_dict:
+                    group_dict[group] = [(chapter, filename)]
+                else:
+                    group_dict[group].append((chapter, filename))
+        return group_dict
+
+    def rename_group(self, dirpath):
+        error = False
+        if dirpath is None or dirpath == "":
+            messagebox.showerror(
+                title="No directory specified", message="Please specify directory!"
+            )
+            return True
+        group_dict = self.get_group_dict(dirpath)
+        if not group_dict:
+            messagebox.showerror(
+                title="No videos found",
+                message="No compatible videos found in provided directory!",
+            )
+            return True
+        print(group_dict)
+        prev_group_prefix = None
+        for group, file_list in group_dict.items():
+            group_prefix = prev_group_prefix if prev_group_prefix else group
+
+            # 1. Check if user wants to provide custom input per group
+            if self.get_user_input_per_group:
+                print("Checking user input per flag")
+                group_prefix = simpledialog.askstring(
+                    title="Custom prefix for renaming",
+                    prompt=(
+                        "Found new video group (starting at path "
+                        f"{os.path.join(dirpath, group_dict[group][0][1])})."
+                        " Replace initial value if you need custom prefix"
+                    ),
+                    initialvalue=group_prefix,
+                )
+                prev_group_prefix = group_prefix if group_prefix != group else None
+                if not group_prefix:
+                    # User pressed cancel
+                    print("Operation canceled. Exiting")
+                    break
+
+            # 2. Create new directory if required
+            new_directory = dirpath
+            if self.create_new_dir_per_group:
+                print("Creating new dir per flag")
+                new_directory = os.path.join(dirpath, group_prefix)
+                try:
+                    os.mkdir(new_directory)
+                except:
+                    merge_existing = "yes"
+
+                    if self.check_dir_merge:
+                        print("Creating new dir per flag")
+                        merge_existing = messagebox.askquestion(
+                            title="Directory exists",
+                            message=f"Found existing directory at {new_directory}. Do you want to merge contents?",
+                        )
+                    if merge_existing == "no":
+                        # TODO - Currently just skipping group on canceling.
+                        # Need to enter UI loop for getting an alternative path
+                        print(f"Operation Canceled. Skipping group {group}")
+                        continue
+
+            # 3. Now copy all video files into selected directory!
+            new_chapter = None
+            for chapter, filename in file_list:
+                # Chapter needs to be bumped up to the max chapter in the new dir if overlapping files exist
+                new_filename = (
+                    f"{group_prefix}_{chapter}.MP4"
+                    if new_chapter is None
+                    else f"{group_prefix}_{new_chapter}.MP4"
+                )
+                source_path = os.path.join(dirpath, filename)
+                dest_path = os.path.join(new_directory, new_filename)
+                if os.path.exists(dest_path):
+                    overwrite_existing = "no"
+                    if self.check_automated_numbering:
+                        overwrite_existing = messagebox.askquestion(
+                            title="File exists",
+                            message=f"Found existing video at {dest_path}. "
+                            f"Do you want to overwrite it with {source_path}?"
+                            "\nIf you select no, an automated indexing scheme will be used",
+                        )
+                    else:
+                        print("Using automated numbering per flag")
+                    if overwrite_existing == "no":
+                        # new_filename needs to be set to one index higher than the existing files
+                        existing_group_dict = self.get_group_dict(
+                            new_directory,
+                            rf"({group_prefix})_([0-9]+).MP4",
+                            reverse_group=True,
+                        )
+                        print(existing_group_dict)
+                        # Find highest index
+                        value_tuples = list(existing_group_dict.values())[0]
+
+                        max_index = max(
+                            [int(group_tuple[0]) for group_tuple in value_tuples]
+                        )
+                        new_chapter = max_index + 1
+                        new_filename = (
+                            f"{group_prefix}_{chapter}.MP4"
+                            if new_chapter is None
+                            else f"{group_prefix}_{new_chapter}.MP4"
+                        )
+                        dest_path = os.path.join(new_directory, new_filename)
+
+                        if os.path.exists(dest_path):
+                            # TODO - Currently just skipping group on canceling.
+                            # Need to enter UI loop for getting an alternative path
+                            print(f"Internal Error. Exiting")
+                            break
+
+                    print(dest_path)
+                if self.copy_instead_of_move:
+                    shutil.copy2(source_path, dest_path)
+                else:
+                    print("Moving per flag")
+                    shutil.move(source_path, dest_path)
+
+                if new_chapter:
+                    new_chapter = new_chapter + 1
+        return error
+
+
+# Tkinter gui for interactive folder utility
+root = tk.Tk()
+# root.withdraw()
+root.geometry("")
+root.title("VidNamer")
+# Code from: https://stackoverflow.com/questions/51877124/how-to-select-a-directory-and-store-it-into-a-variable-in-tkinter/51877299
+def getVideoDirectory():
+    directory = filedialog.askdirectory()
+    videoDirectory.set(directory)
+
+
+def renameVideos():
+    btnBrowse.config(state="disabled")
+    btnStartRename.config(state="disabled")
+    app.check_automated_numbering = not bool(use_automated_numbering_scheme.get())
+    app.check_dir_merge = bool(check_before_dir_merge.get())
+    app.get_user_input_per_group = bool(user_input_per_group.get())
+    app.copy_instead_of_move = bool(copy_instead_of_move.get())
+    app.create_new_dir_per_group = bool(new_dir_per_group.get())
+    error = app.rename_group(videoDirectory.get())
+
+    if not error:
+        messagebox.showinfo(root, message="Videos Renamed!")
+    btnBrowse.config(state="enabled")
+    btnStartRename.config(state="enabled")
+
+
+videoDirectory = StringVar()
+enter_dir_label = Label(root, text="Enter directory").grid(row=0, column=0)
+enter_dir_textbox = Entry(root, textvariable=videoDirectory).grid(row=0, column=1)
+
+
+# Browse Button
+btnBrowse = ttk.Button(root, text="Browse Directory", command=getVideoDirectory)
+btnBrowse.grid(row=0, column=3)
+
+
+# Check boxes for renaming Options
+
+# Check for user input per new group detected. If disabled, VidNamer will reverse 
+# chapter and group and automatically generate new series.
+# Defaults to True
+user_input_per_group = BooleanVar(value=True)
+ttk.Checkbutton(
+    root, text="Ask for custom prefix per video group", variable=user_input_per_group
+).grid(row=1, column=1)
+
+# Check if user wants to create a new directory per video group. Defaults to true.
+new_dir_per_group = BooleanVar(value=True)
+ttk.Checkbutton(
+    root, text="Create a new directory per video group", variable=new_dir_per_group
+).grid(row=2, column=1)
+
+# Check if user wants to make a copy instead of moving
+copy_instead_of_move = BooleanVar(value=True)
+ttk.Checkbutton(
+    root,
+    text="Copy videos instead of moving\n[Recommended if not backed up]",
+    variable=copy_instead_of_move,
+).grid(row=3, column=1)
+
+
+# Check if user wants to check before merging directories
+use_automated_numbering_scheme = BooleanVar(value=True)
+ttk.Checkbutton(
+    root,
+    text="Use auto numbering scheme for filename conflicts",
+    variable=use_automated_numbering_scheme,
+).grid(row=4, column=1)
+
+# Check if user wants to check before merging directories
+check_before_dir_merge = BooleanVar(value=False)
+ttk.Checkbutton(
+    root,
+    text="Ask before writing to an existing directory",
+    variable=check_before_dir_merge,
+).grid(row=5, column=1)
+
+# Rename Button
+btnStartRename = ttk.Button(root, text="Start Renaming!", command=renameVideos)
+btnStartRename.grid(row=6, column=1)
+
+app = VidNamer()
+
+root.mainloop()
